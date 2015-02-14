c4169d025a9084aa84d068680a590fe9ca607389
Birunthan Mohanathas <birunthan@mohanathas.com>
2015-02-14 21:44:40 +0200

    Tweak compilation options

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-clang main.c graphics_control.c -o gpuctl -std=c11 -Wall -framework IOKit
+clang main.c graphics_control.c \
+  -o gpuctl \
+  -DNDEBUG -O2 \
+  -std=c11 -Wall -framework IOKit
